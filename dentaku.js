f5153cc683af61a918245308868d73790ce50ba4
EC2 Default User <ec2-user@ip-172-31-4-149.ec2.internal>
2022-01-16 12:23:24 +0000

    電卓課題

---
+++ dentaku.js
@@ -0,0 +1,19 @@
+
+
+function get_calc(btn) {
+  if(btn.value == "=") {
+   document.dentaku.display.value = eval(document.dentaku.display.value);
+  }else if(btn.value == "C"){
+    document.dentaku.display.value = "";
+  }else {
+    if(btn.value == "×"){
+      btn.value= "*";
+    }else if(btn.value == "÷"){
+      btn.value="/";
+    }
+    document.dentaku.display.value += btn.value;
+    document.dentaku.multi_btn.value = "×";
+    document.dentaku.div_btn.value = "÷";
+  }
+}
+
